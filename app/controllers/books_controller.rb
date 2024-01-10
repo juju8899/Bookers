71f6956336cd69ef01980dd65c3a0b999b891ab5
juju8899 <146299878+juju8899@users.noreply.github.com>
2024-01-10 11:31:58 +0000

    editページ追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
      flash[:notice] = "Book was successfully updated."
-     redirect_to book_path (@book.id)
+     redirect_to books_path
     else
      render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # get '/top' => 'homes#top'
   root to: 'homes#top'
-  resources :books
+  resources :books, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
